5e41289360a69ab171df076d11000c9e7edc1de0
Mario <mamerida2013@gmail.com>
2022-05-09 13:00:06 -0300

    work with WS message

--- conectionManager.js
+++ conectionManager.js
@@ -10,16 +10,35 @@ const socket = new WebSocket(`wss://4yyity02md.execute-api.us-east-1.amazonaws.c
 
 //make listener
 socket.on('open', function open() {
-    console.log("Im in");
+
+    
+    
+});
+
+//catch message
+socket.on('message', function catchMessage(data) {
+    //convert message to JSON to work with him
+    let message = JSON.parse(data) ;
+    switch (message.event) {
+        case "challenge":
+            console.log('i recibe a challenger')
+    }
 });
 
 
 
 
 
+
+//catch error
 socket.on('error', function error(e) {
     console.log("Error : ",e)
 });
+
+//when close WS connection
+socket.on('close', function close() {
+    console.log('disconnected');
+  });
 
 
 
