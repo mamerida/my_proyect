cff9848400addf5089013cebcb66d3d94bf7e126
mamerida <mamerida2018@gmail.com>
2022-05-20 17:14:59 -0300

    refactor viewLine

--- utils/viewLine.js
+++ utils/viewLine.js
@@ -12,30 +12,46 @@ exports.viewLine = (nearestPawn,board,other_side) =>{
     }
     let countEmptySquare = 0;
     let thereAreWall = true;
+    
     if(other_side =="N"){
+        const rowToSearch =  +1;
+    }else{
+        const rowToSearch =  -1;
+    }
+
+    board[nearestPawn.row + rowToSearch].forEach(row => {row == " "  && countEmptySquare++});
+
         //i cant block the way of the pawn. So look the row and view the number of empty spaces
-        board[nearestPawn.row + 1].forEach(row => {
-            row == " "  && countEmptySquare++  
-        });
-        if(board[nearestPawn.row + 1][nearestPawn.column - 1] != " " && 
-            board[nearestPawn.row + 1][nearestPawn.column - 2] != " " && 
-            board[nearestPawn.row + 1][nearestPawn.column - 3] != " "){
+        if(board[nearestPawn.row + rowToSearch][nearestPawn.column - 1] != " " && 
+            board[nearestPawn.row + rowToSearch][nearestPawn.column - 2] != " " && 
+            board[nearestPawn.row + rowToSearch][nearestPawn.column - 3] != " "){
 
                 thereAreWall = false;
         }
-    }
 
-    if(other_side =="S"){
-        board[nearestPawn.row - 1].forEach(row => {  
-            row == " "  && countEmptySquare++  
-        });
-        if(board[nearestPawn.row - 1][nearestPawn.column -1] != " " &&
-            board[nearestPawn.row - 1][nearestPawn.column - 2] != " " &&
-            board[nearestPawn.row - 1][nearestPawn.column - 3] != " " ){
+    // if(other_side =="N"){
+    //     // //i cant block the way of the pawn. So look the row and view the number of empty spaces
+    //     board[nearestPawn.row + 1].forEach(row => {row == " "  && countEmptySquare++});
 
-                thereAreWall = false;
-        }  
-    }
+    //     if(board[nearestPawn.row + 1][nearestPawn.column - 1] != " " && 
+    //         board[nearestPawn.row + 1][nearestPawn.column - 2] != " " && 
+    //         board[nearestPawn.row + 1][nearestPawn.column - 3] != " "){
+
+    //             thereAreWall = false;
+    //     }
+    // }
+
+    // if(other_side =="S"){
+    //     board[nearestPawn.row - 1].forEach(row => {  
+    //         row == " "  && countEmptySquare++  
+    //     });
+    //     if(board[nearestPawn.row - 1][nearestPawn.column -1] != " " &&
+    //         board[nearestPawn.row - 1][nearestPawn.column - 2] != " " &&
+    //         board[nearestPawn.row - 1][nearestPawn.column - 3] != " " ){
+
+    //             thereAreWall = false;
+    //     }  
+    // }
 
     // if i block row i wont block the row 
     if(countEmptySquare < 4 || !thereAreWall ){
