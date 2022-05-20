fa7f1b4ae21d7aa31ecf3f801407d315e256ce7c
mamerida <mamerida2018@gmail.com>
2022-05-20 17:21:05 -0300

    refactor near pawn

--- utils/nearPawn.js
+++ utils/nearPawn.js
@@ -10,21 +10,30 @@ exports.nearPawn= (position,side) =>{
 
     let nearPown = position[0]
     //verify if my opponet have S i will put a wall front of  pawn with smaller row
-    if(side == "S"){
-        position.forEach(pawn => {
-            if( nearPown.column == 0 || nearPown.row == 16 || nearPown.row > pawn.row){
+    position.forEach(pawn => {
+        if( side == "S" &&( nearPown.column == 0 || nearPown.row == 16 || nearPown.row > pawn.row)){
+            nearPown = pawn;
+        }else if(side == "N" && (nearPown.column == 0 || nearPown.row  == 0 || nearPown.row < pawn.row)){
                 nearPown = pawn;
-            }
-        });
-    }
+        }
+    });
 
-    if(side == "N"){
-        position.forEach(pawn => {
-            if(nearPown.column == 0 || nearPown.row  == 0 || nearPown.row < pawn.row){
-                nearPown = pawn;
-            }
-        });
-    }
+    // if(side == "S"){
+    //     position.forEach(pawn => {
+    //         if( nearPown.column == 0 || nearPown.row == 16 || nearPown.row > pawn.row){
+    //             nearPown = pawn;
+    //         }
+    //     });
+    // }
+
+    // if(side == "N"){
+    //     position.forEach(pawn => {
+    //         if(nearPown.column == 0 || nearPown.row  == 0 || nearPown.row < pawn.row){
+    //             nearPown = pawn;
+    //         }
+    //     });
+    // }
+
 
     return nearPown;
 
