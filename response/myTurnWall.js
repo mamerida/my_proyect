90f09201a29410585138c8d2d5d2b82cec5ecec7
mamerida <mamerida2018@gmail.com>
2022-05-20 19:14:55 -0300

    implements vertical walls

--- response/myTurnWall.js
+++ response/myTurnWall.js
@@ -28,12 +28,28 @@ exports.myTurnWall = (message) =>{
     let nearestPawn  = nearPawn(position,other_side);
     
     //comprobe if i can put a wall front of selected pawn
-    const isClear  = viewLine(nearestPawn,board,other_side); // return true or false 
+    let isClear  = viewLine(nearestPawn,board,other_side); // return true or false 
+    let countFullSqueare = 0;
 
     if(!isClear){
         let newPositios = position.filter(pawn => pawn.row != nearestPawn.row && pawn.column != nearestPawn.column );
         nearestPawn  = nearPawn(newPositios,other_side);
+        isClear = viewLine(nearestPawn,board,other_side);
+        countFullSqueare++;
     }
+
+    //if i cant putl horizontal wall  I will put vertical call
+    let finalPositionVertical = "";
+    if(countFullSqueare>=2){
+        const verticialNearWalls = nearPawn(position,other_side);
+        finalPositionVertical  = viewVerticalLine(verticialNearWalls,board,other_side); // return final position
+
+    }
+
+    if(finalPositionVertical){
+        return putWall(message,parseInt((finalPositionVertical.row /2)- 1) ,parseInt(finalPositionVertical.column/2),"v")
+    }
+
 
     if(other_side == "S"){
         //message,row,col,direction
--- utils/viewLine.js
+++ utils/viewLine.js
@@ -7,45 +7,21 @@ exports.viewLine = (nearestPawn,board,other_side) =>{
     let countEmptySquare = 0;
     let thereAreWall = true;
     
+    let rowToSearch  = 0;
     if(other_side =="N"){
-        const rowToSearch =  +1;
+        rowToSearch = + 1;
     }else{
-        const rowToSearch =  -1;
+        rowToSearch = - 1;
     }
 
+    //i cant block the way of the pawn. So look the row and view the number of empty spaces
     board[nearestPawn.row + rowToSearch].forEach(row => {row == " "  && countEmptySquare++});
 
-        //i cant block the way of the pawn. So look the row and view the number of empty spaces
-        if(board[nearestPawn.row + rowToSearch][nearestPawn.column - 1] != " " && 
-            board[nearestPawn.row + rowToSearch][nearestPawn.column - 2] != " " && 
-            board[nearestPawn.row + rowToSearch][nearestPawn.column - 3] != " "){
-
-                thereAreWall = false;
-        }
-
-    // if(other_side =="N"){
-    //     // //i cant block the way of the pawn. So look the row and view the number of empty spaces
-    //     board[nearestPawn.row + 1].forEach(row => {row == " "  && countEmptySquare++});
-
-    //     if(board[nearestPawn.row + 1][nearestPawn.column - 1] != " " && 
-    //         board[nearestPawn.row + 1][nearestPawn.column - 2] != " " && 
-    //         board[nearestPawn.row + 1][nearestPawn.column - 3] != " "){
-
-    //             thereAreWall = false;
-    //     }
-    // }
-
-    // if(other_side =="S"){
-    //     board[nearestPawn.row - 1].forEach(row => {  
-    //         row == " "  && countEmptySquare++  
-    //     });
-    //     if(board[nearestPawn.row - 1][nearestPawn.column -1] != " " &&
-    //         board[nearestPawn.row - 1][nearestPawn.column - 2] != " " &&
-    //         board[nearestPawn.row - 1][nearestPawn.column - 3] != " " ){
-
-    //             thereAreWall = false;
-    //     }  
-    // }
+    //Is there other wall ?    
+    if(board[nearestPawn.row + rowToSearch][nearestPawn.column - 1] != " " && board[nearestPawn.row + rowToSearch][nearestPawn.column - 2] != " " && 
+        board[nearestPawn.row + rowToSearch][nearestPawn.column - 3] != " "){
+            thereAreWall = false;
+    }
 
     // if i block row i wont block the row 
     if(countEmptySquare < 4 || !thereAreWall ){
