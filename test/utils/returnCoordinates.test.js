b8e84af3054b37cef5c7e23bc19340508e66b514
mamerida <mamerida2018@gmail.com>
2022-05-22 08:47:08 -0300

    upgrade test

--- test/utils/returnCoordinates.test.js
+++ test/utils/returnCoordinates.test.js
@@ -4,22 +4,22 @@ describe(" return moves to message" , ()=>{
 
     test("with row 0 column 0 and move up", ()=>{
         //[to_row,to_col]
-        expect(returnCoordinates({moves:{up:"up"}},0,0)).toStrictEqual([1,0]);
+        expect(returnCoordinates({moves:{up:"up",left:"left",down:"down"}},0,0)).toStrictEqual([1,0]);
     })
 
     test("with row 0 column 0 and move upJump", ()=>{
         //[to_row,to_col]
-        expect(returnCoordinates({moves:{upJump:"upJump"}},0,0)).toStrictEqual([2,0]);
+        expect(returnCoordinates({moves:{up:"up",upJump:"upJump"}},0,0)).toStrictEqual([2,0]);
     })
 
     test("with row 4 column 4 and move upLeft", ()=>{
         //[to_row,to_col]
-        expect(returnCoordinates({moves:{upLeft:"upLeft"}},4,4)).toStrictEqual([5,5]);
+        expect(returnCoordinates({moves:{up:"up",upLeft:"upLeft"}},4,4)).toStrictEqual([5,5]);
     })
 
     test("with row 4 column 4 and move upRigth", ()=>{
         //[to_row,to_col]
-        expect(returnCoordinates({moves:{upRigth:"upRigth"}},4,4)).toStrictEqual([5,3]);
+        expect(returnCoordinates({moves:{up:"up",upRigth:"upRigth"}},4,4)).toStrictEqual([5,3]);
     })
 
     test("with row 4 column 4 and move left", ()=>{
--- test/utils/selectBestPawn.test.js
+++ test/utils/selectBestPawn.test.js
@@ -3,7 +3,7 @@ const {bestPawn} = require('../../utils/selectBestPawn');
 describe(" select the pawn closest to the opposite line " , ()=>{
 
     const powns = [{row:0 ,column:6},{row:4 ,column:6},{row:8 ,column:6}]
-    const powsEqualsValues= [{row:2 ,column:6},{row:2 ,column:6},{row:2 ,column:6}]
+    const powsEqualsValues= [{row:2 ,column:4},{row:2 ,column:6},{row:2 ,column:8}]
 
     test("with out message", ()=>{
         expect(()=>{bestPawn()}).toThrow(new Error("Error need all parameters"))
@@ -14,7 +14,7 @@ describe(" select the pawn closest to the opposite line " , ()=>{
     })
 
     test("pawns with equuals row", ()=>{
-        expect(bestPawn(powsEqualsValues)).toStrictEqual({row:2 ,column:6});
+        expect(bestPawn(powsEqualsValues)).toStrictEqual({row:2 ,column:4});
     })
 
 })
